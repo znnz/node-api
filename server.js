240b4d4629a307bf0bd0b2dc4f7e7e448a5a4f7c
znnz <jojokyi21@gmail.com>
2015-12-11 16:14:39 +0800

    DELETE http verb Request

--- server.js
+++ server.js
@@ -75,6 +75,12 @@ apiRouter.route('/users/:user_id')
 				res.json({message:"User updated!!!!"});
 			});
 		});
+	})
+	.delete(function(req,res){
+		User.remove({_id:req.params.user_id},function(err,user){
+			if(err) return res.send(err);
+			res.json({message:'Successfully deleted'});
+		});
 	});
 
 app.listen(port);
